0c543dcfd6739145188b52bbc6af5550de346a96
Alex Fernandes da Silva <alex.rgracie@gmail.com>
2023-06-08 22:35:34 -0300

    Aprofundamento Express

--- typescript-i/src/index.ts
+++ typescript-i/src/index.ts
@@ -50,4 +50,66 @@ app.post('/products', (req: Request, res: Response)=> {
     res.status(201).send('Produto cadastrado com sucesso')
 }) 
 
-  
+app.delete('/users/:id', (req: Request, res: Response)=>{
+  const { id } = req.params
+
+  // Validação do parâmetro de ID
+  if (!id) {
+    res.status(400).send('ID inválido');
+    return;
+  }
+
+  const indexUserToDelete = users.findIndex(user => user.id === id)
+
+  if(indexUserToDelete < 0){
+    res.status(404).send('Usuário não encontrado')
+    return
+  }
+
+  users.splice(indexUserToDelete, 1)
+  res.status(200).send('Usuário deletado com sucesso')
+})
+
+app.delete('/products/:id', (req: Request, res: Response)=>{
+  const { id } = req.params
+
+  // Validação do parâmetro de ID
+  if (!id) {
+    res.status(400).send('ID inválido');
+    return;
+  }
+
+  const indexProductToDelete = products.findIndex(product => product.id === id)
+
+  if(indexProductToDelete < 0){
+    res.status(404).send('Produto não encontrado')
+    return
+  }
+
+  products.splice(indexProductToDelete, 1)
+  res.status(200).send('Produto deletado com sucesso')
+})
+
+
+app.put('/products/:id', (req: Request, res: Response)=>{
+  const { id } = req.params 
+
+  const newId = req.body.id as string | undefined
+  const newName = req.body.name as string | undefined
+  const newPrice = req.body.price as number | undefined
+  const newDescription = req.body.description as string | undefined
+  const newImageUrl = req.body.imageUrl as string | undefined
+
+  const productToUpdate = products.find((product) =>product.id === id)
+
+  if(productToUpdate){
+    productToUpdate.id = newId || productToUpdate.id
+    productToUpdate.name = newName || productToUpdate.name
+    productToUpdate.price = newPrice || productToUpdate.price
+    productToUpdate.description = newDescription || productToUpdate.description
+    productToUpdate.imageUrl = newImageUrl || productToUpdate.imageUrl
+  }
+
+  res.status(200).send("Produto atuzalido com sucesso")
+
+})
