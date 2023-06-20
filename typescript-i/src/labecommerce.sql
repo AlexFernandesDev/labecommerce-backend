29430cfd537906899f2fadaa1c43e2a9830add7e
Alex Fernandes da Silva <alex.rgracie@gmail.com>
2023-06-20 16:33:17 -0300

    aprofundamento sql

--- typescript-i/src/labecommerce.sql
+++ typescript-i/src/labecommerce.sql
@@ -1,4 +1,4 @@
--- Active: 1687132479182@@127.0.0.1@3306
+-- Active: 1687280178194@@127.0.0.1@3306
 CREATE TABLE users (
     id TEXT PRIMARY KEY NOT NULL,
     email TEXT NOT NULL,
@@ -11,7 +11,6 @@ VALUES
 ('u006','user6@gmail.com','1234'),
 ('u007','user7@gmail.com','1234');
 
-SELECT * FROM users;
 
 CREATE TABLE products (
     id TEXT PRIMARY KEY NOT NULL,
@@ -19,8 +18,6 @@ CREATE TABLE products (
     price REAL NOT NULL,
     category TEXT NOT NULL
 );
-
-PRAGMA table_info('products');
 
 INSERT INTO products (id, name, price, category)    
 VALUES
@@ -30,4 +27,57 @@ VALUES
 ('prod006','Camiseta',25.90,'vestuário'),
 ('prod007','Fone de ouvido',110.15,'eletronicos');
 
+-- Busca por todos os usuários
+SELECT * FROM users;
+
+-- Busca por todos os produtos
+SELECT * FROM products;
+
+-- Busca um produto pelo nome
+SELECT * FROM products WHERE name = 'Celular';
+
+-- Cria um novo usuário
+INSERT INTO users (id, email, password)
+VALUES
+('u008','user8@gmail.com','1234');
+
+-- Cria um novo produto
+INSERT INTO products (id, name, price, category)
+VALUES
+('prod008','Smartphone',1200.95,'telefonia');
+
+-- Buscar produto pelo id   
 SELECT * FROM products
+WHERE id = 'prod008';
+
+-- Deletar user pelo id
+DELETE FROM users
+WHERE id = 'u008';
+
+-- Deletar product pelo id 
+DELETE FROM products
+WHERE id = 'prod008';
+
+-- Atualizar user pelo id - atualizar todas as colunas
+UPDATE users
+SET email = '<EMAIL>', password = '<PASSWORD>'
+WHERE id = 'u008';
+
+-- Atualizar produto pelo id - atualizar todas as colunas
+UPDATE products
+SET name = '<NAME>', price = '<PRICE>', category = '<CATEGORY>'
+WHERE id = 'prod008';
+
+-- Buscar todos os users ordenando pela coluna email em ordem crescente
+SELECT * FROM users
+ORDER BY email ASC;
+
+-- Buscar todos os produtos ordenando pela coluna price em ordem crescente, limitando o resultado em 3
+SELECT * FROM products 
+ORDER BY price ASC
+LIMIT 3;
+
+-- Buscar todos os produtos com intervalo de preço entre 100 e 300 em ordem crescente
+SELECT * FROM products
+WHERE price BETWEEN 100 AND 300
+ORDER BY price ASC;
