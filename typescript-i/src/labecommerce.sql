371a5f3e53d39364e3dcc52d15a8955da78cac95
Alex Fernandes da Silva <alex.rgracie@gmail.com>
2023-06-26 22:34:26 -0300

    relacoes-sql-i

--- typescript-i/src/labecommerce.sql
+++ typescript-i/src/labecommerce.sql
@@ -1,15 +1,17 @@
 -- Active: 1687280178194@@127.0.0.1@3306
 CREATE TABLE users (
     id TEXT PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
     email TEXT NOT NULL,
-    password TEXT NOT NULL
+    password TEXT NOT NULL,
+    createdAt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO users (id, email, password)
+INSERT INTO users (id, name, email, password)
 VALUES
-('u005','user5@gmail.com','1234'),
-('u006','user6@gmail.com','1234'),
-('u007','user7@gmail.com','1234');
+('u005', 'Pedro','user5@gmail.com','1234'),
+('u006', 'Marcelo', 'user6@gmail.com','1234'),
+('u007', 'Otavio', 'user7@gmail.com','1234');
 
 
 CREATE TABLE products (
@@ -37,9 +39,9 @@ SELECT * FROM products;
 SELECT * FROM products WHERE name = 'Celular';
 
 -- Cria um novo usuário
-INSERT INTO users (id, email, password)
+INSERT INTO users (id, name, email, password)
 VALUES
-('u008','user8@gmail.com','1234');
+('u008', 'Maria', 'user8@gmail.com','1234');
 
 -- Cria um novo produto
 INSERT INTO products (id, name, price, category)
@@ -81,3 +83,38 @@ LIMIT 3;
 SELECT * FROM products
 WHERE price BETWEEN 100 AND 300
 ORDER BY price ASC;
+
+-- Criação da tabela de pedidos
+CREATE TABLE purchases (
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    buyer TEXT NOT NULL,
+    total_price REAL NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (buyer) REFERENCES users(id)
+);
+
+INSERT INTO purchases (id, buyer, total_price)
+VALUES
+    ('p001', 'u005', 1.750),
+    ('p002', 'u006', 275),
+    ('p003', 'u007', 582);
+
+
+SELECT * FROM purchases;
+
+UPDATE purchases
+SET total_price = 120
+WHERE id = 'p001';
+
+-- INNER JOIN entre purchases e users com as colunas purchase id, buyer id, buyer name, email, total price e created_at
+SELECT 
+    purchases.id AS purchaseID, 
+    purchases.buyer AS buyerID, 
+    users.name, 
+    users.email, 
+    purchases.total_price, 
+    purchases.created_at
+FROM purchases
+INNER JOIN users
+ON purchases.buyer = users.id;
+
