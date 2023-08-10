a0f3962ff634470370f7cf8f2311a58ac8fca6e2
Alex Fernandes da Silva <alex.rgracie@gmail.com>
2023-08-10 01:57:23 -0300

    finalizado main

---
+++ labecommerce/src/database/labecommerce.sql
@@ -0,0 +1,38 @@
+-- Active: 1688324221972@@127.0.0.1@3306
+-- Criação da tabela de usuários
+CREATE TABLE users (
+    id TEXT PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Criação da tabela de produtos
+CREATE TABLE products (
+    id TEXT PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+    price REAL NOT NULL,
+    description TEXT NOT NULL,
+    image_url TEXT NOT NULL
+);
+
+-- Criação da tabela de pedidos
+CREATE TABLE purchases (
+    id TEXT PRIMARY KEY UNIQUE NOT NULL,
+    buyer TEXT NOT NULL,
+    total_price REAL NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    paid BOOLEAN,
+    FOREIGN KEY (buyer) REFERENCES users(id)
+);
+
+-- Criação da tabela de relação entre pedidos e produtos
+CREATE TABLE purchases_products (
+    purchase_id TEXT NOT NULL,
+    product_id TEXT NOT NULL,
+    quantity INTEGER NOT NULL,
+    FOREIGN KEY (purchase_id) REFERENCES purchases(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
+);
+
