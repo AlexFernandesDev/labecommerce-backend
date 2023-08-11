fd00c50c0f48d96a72f1b75ff32e7c6179ff5419
Alex Fernandes da Silva <alex.rgracie@gmail.com>
2023-08-10 21:51:49 -0300

    Finalizado main

--- labecommerce/src/database/labecommerce.sql
+++ labecommerce/src/database/labecommerce.sql
@@ -1,4 +1,4 @@
--- Active: 1688324221972@@127.0.0.1@3306
+-- Active: 1691714031743@@127.0.0.1@3306
 -- Criação da tabela de usuários
 CREATE TABLE users (
     id TEXT PRIMARY KEY NOT NULL,
@@ -8,6 +8,14 @@ CREATE TABLE users (
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Inserções na tabela de usuários
+INSERT INTO users (id, name, email, password)
+VALUES
+    ('user1', 'João Silva', 'joao@example.com', 'hashed_password_123'),
+    ('user2', 'Maria Souza', 'maria@example.com', 'hashed_password_456'),
+    ('user3', 'Carlos Oliveira', 'carlos@example.com', 'hashed_password_789');
+
+
 -- Criação da tabela de produtos
 CREATE TABLE products (
     id TEXT PRIMARY KEY NOT NULL,
@@ -16,6 +24,14 @@ CREATE TABLE products (
     description TEXT NOT NULL,
     image_url TEXT NOT NULL
 );
+
+-- Inserções na tabela de produtos
+INSERT INTO products (id, name, price, description, image_url)
+VALUES
+    ('product1', 'Camiseta Branca', 29.99, 'Camiseta de algodão branca', 'image_url1'),
+    ('product2', 'Calça Jeans', 59.99, 'Calça jeans tradicional', 'image_url2'),
+    ('product3', 'Tênis Esportivo', 79.99, 'Tênis para atividades esportivas', 'image_url3');
+
 
 -- Criação da tabela de pedidos
 CREATE TABLE purchases (
@@ -27,6 +43,14 @@ CREATE TABLE purchases (
     FOREIGN KEY (buyer) REFERENCES users(id)
 );
 
+-- Inserções na tabela de pedidos
+INSERT INTO purchases (id, buyer, total_price, paid)
+VALUES
+    ('purchase1', 'user1', 89.98, TRUE),
+    ('purchase2', 'user2', 59.99, TRUE),
+    ('purchase3', 'user3', 109.98, FALSE);
+
+
 -- Criação da tabela de relação entre pedidos e produtos
 CREATE TABLE purchases_products (
     purchase_id TEXT NOT NULL,
@@ -35,4 +59,14 @@ CREATE TABLE purchases_products (
     FOREIGN KEY (purchase_id) REFERENCES purchases(id),
     FOREIGN KEY (product_id) REFERENCES products(id)
 );
+
+-- Inserções na tabela de relação entre pedidos e produtos
+INSERT INTO purchases_products (purchase_id, product_id, quantity)
+VALUES
+    ('purchase1', 'product1', 2),
+    ('purchase1', 'product3', 1),
+    ('purchase2', 'product2', 1),
+    ('purchase3', 'product1', 3),
+    ('purchase3', 'product2', 1);
+
 
